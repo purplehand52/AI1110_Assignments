25b0835556f5330a58c707506ba004057818a6c0
purplehand52 <102431650+purplehand52@users.noreply.github.com>
2022-05-24 10:00:37 +0530

    Update coin_prob.py

--- Assignment7/PyCode/coin_prob.py
+++ Assignment7/PyCode/coin_prob.py
@@ -1,13 +1,15 @@
 import numpy as np
 
+#Sample Space
 PROB_RAND = 10000
 SAMPLE_RAND = 10000
 
+#Uniform RV for probability
 prob = np.random.rand(PROB_RAND)
 prob = (prob*0.2) + 0.4
 
+#Values for finding number of heads out of sample space and assigning Boolean values
 sample = np.random.rand(SAMPLE_RAND, PROB_RAND)
-
 chk = sample < prob
 
 ans = np.count_nonzero(chk)/(SAMPLE_RAND*PROB_RAND)
